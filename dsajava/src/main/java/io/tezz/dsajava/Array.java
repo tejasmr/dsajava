4a37830ab9fa8c76969f7ba9c73718443a34acbe
Tejas M R <tejasmr@outlook.com>
2023-05-26 13:50:55 +0000

    Added initializer for Array class

--- dsajava/src/main/java/io/tezz/dsajava/Array.java
+++ dsajava/src/main/java/io/tezz/dsajava/Array.java
@@ -1,7 +1,15 @@
 package io.tezz.dsajava;
 
 public class Array {
-    public Array() {
-        
+    private int length;
+    private int[] container;
+
+    public Array(int length) {
+        this.length = length;
+        this.container = new int[this.length];
+    }
+
+    public int getLength() {
+        return this.length;
     }
 }
