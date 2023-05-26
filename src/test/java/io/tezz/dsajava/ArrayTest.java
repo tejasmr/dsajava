233145e3269b24ff45d02c9df82b6d9308cfdc25
Tejas M R <tejasmr@outlook.com>
2023-05-26 14:18:29 +0000

    moved one test and added three tests

--- src/test/java/io/tezz/dsajava/ArrayTest.java
+++ src/test/java/io/tezz/dsajava/ArrayTest.java
@@ -7,9 +7,34 @@ import org.junit.Test;
 public class ArrayTest {
 
     @Test 
-    public void createEmptyArray() {
+    public void createdEmptyArrayIsNotNull() {
         Array array = new Array(0);
         assertFalse(array == null);
+    }
+
+    @Test
+    public void emptyArrayIsOfLengthZero() {
+        Array array = new Array(0);
         assertEquals(array.getLength(), 0);
     }
+
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void emptyArrayThrowsExGetIndexZero() {
+        Array array = new Array(0);
+        array.get(0);
+    }
+
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void emptyArrayThrowsExGetIndexNegative() {
+        Array array = new Array(0);
+        array.get(-1);
+    }
+
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void emptyArrayThrowsExGetIndexPositive() {
+        Array array = new Array(0);
+        array.get(1);
+    }
+
+
 }
